c6d4bac5503e53d6b452b7f802ec9d4461f7179f
concalveRose <conclaveyohan.l@gmail.com>
2021-01-13 17:45:06 +0900

    Third Modify main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const alarm = document.querySelector('.js_alarm');
 const currentTime = document.querySelector('.js_alarm h1');
 const setTime = document.querySelector('.alarm_time');
 const bgColor = document.querySelector('body');
+const Btn = document.querySelector('button');
+
 
 // 배경색 설정 함수
 function setbgColor() {
@@ -13,6 +15,11 @@ function setbgColor() {
     }
 };
 
+//알람벨소리 
+function setSound() {
+    const audio = document.querySelector('audio');
+    audio.play();
+}
 
 
 // 알람시간 설정 함수
@@ -22,10 +29,17 @@ function getAlarm() {
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const current = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
-    if (setValue === current) {
-        setbgColor();
-    }
+
+
+    setInterval(() => {
+        if (setValue === current) {
+            setbgColor();
+            setSound();
+        }
+    }, 1000);
+
 };
+
 
 
 //현재시간 설정 함수 
@@ -43,14 +57,27 @@ function init() {
     setInterval(() => {
         getTime();
     }, 1000);
-    setInterval(() => {
-        getAlarm();
-    }, 1000);
 
 }
 
 //함수 호출
 init();
+
+//알람 버튼 핸들러 
+Btn.addEventListener('click', () => {
+    if (Btn.className == 'startBtn') {
+        Btn.innerText = 'Stop';
+        Btn.classList.remove('startBtn');
+        Btn.classList.add('stopBtn');
+        getAlarm();
+    } else {
+        Btn.innerText = 'Start';
+        Btn.classList.remove('stopBtn');
+        Btn.classList.add('startBtn');
+        window.location.reload();
+    }
+})
+
 
 
 
